926fca2c6700046b71ce3ce63cc9aed48d19b022
tstm2110 <ts2021ik06@gmail.com>
2021-11-10 13:25:25 +0000

    Initial Commit

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,13 +1,14 @@
 class BookCommentsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
+	  @books=@book.book_comments
+
 		@book_comment = BookComment.new(book_comment_params)
 		@book_comment.book_id = @book.id
 		@book_comment.user_id = current_user.id
 		if @book_comment.save
-  		redirect_to book_path(@book.id)
+
 		else
-	  @books=@book.book_comments
     @users=User.all
     @user =@book.user
     @bookkara=Book.new
@@ -16,10 +17,14 @@ class BookCommentsController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
+  	@book = Book.find(params[:book_id])
+
+  	@books=@book.book_comments
+
+		@book_comment = BookComment.new
   	book_comment = @book.book_comments.find(params[:id])
 		book_comment.destroy
-		redirect_to request.referer
+
   end
   private
 	def book_comment_params
